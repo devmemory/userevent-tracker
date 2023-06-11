02e90498d4c82b89844358f4fe46c85ebc30a2f8
devmemory <devmemorydh@gmail.com>
2023-06-11 14:43:16 +0900

    - document update

--- example/react/src/route/layout.tsx
+++ example/react/src/route/layout.tsx
@@ -1,17 +1,13 @@
 import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Nav from "src/components/Nav";
-import { storeUtil, tracker } from "../../../../lib/dist";
+import { tracker } from "userevent_tracker";
 
 const Layout = () => {
   useEffect(() => {
     // you can set show console or no. default is hiding
     // you can set uuid to track an user.
     tracker({showConsole: true, customUUID: undefined})
-
-    return () => {
-      storeUtil.getTrackData()
-    }
   }, []);
 
   return (
